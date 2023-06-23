2c3baafbb15e6175d79301ef608b57215a3eb7af
DukAlixandr <79523854+DukAlixandr@users.noreply.github.com>
2023-06-23 22:40:31 +0300

    Add Avatars

--- Assets/GamePlayScene/Scripts/EndTurnScript.cs
+++ Assets/GamePlayScene/Scripts/EndTurnScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndTurnScript : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class EndTurnScript : MonoBehaviour
     {
         
     }
-    public void EndTurn(){
-        
+    public void GoToGDScene(){
+        SceneManager.LoadScene("SceneForGD");
     }
 }
